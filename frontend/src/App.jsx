bc8545f04cc7589331cc2aebddc94f6159ee8177
Saurav-Bhattacharjee <saroh2009@gmail.com>
2024-05-18 11:23:34 +0530

    routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter,Routes,Route} from "react-router-dom" 
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-red-400'>
-        Hello world
-      </div>
-        
+    <BrowserRouter>
+    <Routes>
+      <Route path="home" element={<Home/>}/>
+      <Route path="calculation" element={<Calculation/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
