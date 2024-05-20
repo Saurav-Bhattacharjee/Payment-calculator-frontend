02a07e474956974f79050536a19ba86d2f8057b6
Saurav-Bhattacharjee <saroh2009@gmail.com>
2024-05-20 16:56:51 +0530

    frontpage button added

--- frontend/src/components/Homebody.jsx
+++ frontend/src/components/Homebody.jsx
@@ -2,7 +2,16 @@
 
 export const Homebody =()=>{
     return<div>
-        
+        <div className="flex flex-col items-center">
+            <div className="mb-4">        
+                Want to calculate your  expense?? Click the button below:
+            </div>
+            <div >
+            <button className="  bg-black text-white hover:bg-green-300 font-bold rounded shadow-lg py-2 px-4">
+                click here
+            </button>
+            </div>
+        </div>
 
     </div>
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,9 +7,12 @@ export const Home=()=>{
     return<div>
         <div className="bg-slate-300 h-screen flex justify-center">
             <div>
-                <div className='block h-64 w-96 bg-white p-4 rounded shadow-lg'>
+                <div className='block h-96 w-196 bg-white mt-8 p-4 rounded shadow-lg'>
                     <Heading label={"Your Personal Money Tracker App"}/>
-                    <Homebody/>
+                    <div className="block w-196 h-32 m-6 bg-blue-300">
+                        <Homebody/>
+                    </div>
+                    
 
                 </div>
            
