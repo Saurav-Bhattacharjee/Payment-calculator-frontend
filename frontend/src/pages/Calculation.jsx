00a83813fc0ce69cff39400628e2480c6809acd6
Saurav-Bhattacharjee <saroh2009@gmail.com>
2024-05-28 14:33:08 +0530

    Table Head of the expenditure  created

--- frontend/src/pages/Calculation.jsx
+++ frontend/src/pages/Calculation.jsx
@@ -2,7 +2,22 @@
 
 export const Calculation=()=>{
     return<div>
-        Calculate here
+        <table className="table-auto">
+            <thead>
+                <tr>
+                    <th>Sl No.</th>
+                    <th>Name of the Expenditure</th>
+                    <th>Amount</th>
+                </tr>                
+            </thead>
+            <tbody>
+                <tr></tr>
+                <tr>
+                    <th>Total</th>
+                    <th>---</th>
+                </tr>
+            </tbody>
+        </table>
         
     </div>
 }
